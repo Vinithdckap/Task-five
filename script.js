3ae51d91acc2f724a6188000cd48e5b2d2bb292d
Vinith Krishnan <vinithkdckap@gmail.com>
2023-11-27 22:24:19 +0530

    80% completed the select form only js junctions remaining

---
+++ script.js
@@ -0,0 +1,9 @@
+let addCoupon = document.querySelector(".addCoupon");
+let formDetails = document.querySelectorAll(".formDetails");
+
+
+addCoupon.addEventListener("click",()=>{
+    // console.log(formDetails);
+    
+    
+})
