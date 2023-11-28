c37792315d4f459f3937130e9e59552df9f71f2b
Vinith Krishnan <vinithkdckap@gmail.com>
2023-11-28 14:44:31 +0530

    js functions completed

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-let addCoupon = document.querySelector(".addCoupon");
+let infoLink = document.querySelector(".infoLink")
 let formDetails = document.querySelectorAll(".formDetails");
 
+// let addCoupon = document.querySelector(".offerCode");
+let coupon = document.querySelector(".coupon");
+let offerCode = document.querySelector(".offerCode")
 
-addCoupon.addEventListener("click",()=>{
-    // console.log(formDetails);
-    
-    
+
+
+infoLink.addEventListener("click", () => {
+
+    for (let i = 0; i < formDetails.length; i++) {
+        formDetails[i].classList.toggle("open");
+    }
+
+
+    if (infoLink.innerText == "Add Info") {
+        infoLink.innerText = "Cancel";
+    }
+    else if (infoLink.innerText == "Cancel") {
+        infoLink.innerText = "Add Info";
+    }
+
+
+})
+
+
+offerCode.addEventListener("click", () => {
+
+    coupon.classList.toggle("offerOpen");
+
+    if (offerCode.innerText == "Add Coupon") {
+        offerCode.innerText = "Cancel";
+    }
+    else if (offerCode.innerText == "Cancel") {
+        offerCode.innerText = "Add Coupon"
+    }
 })
